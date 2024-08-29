b90cc77282919ab6662c4225bdba8ac85537f814
bakhromov <najottalim@gmail.com>
2024-08-29 11:47:33 +0500

    first commit

--- shop/signals.py
+++ shop/signals.py
@@ -1,46 +1,9 @@
 import os
-
-from django.core.mail import send_mail
 from django.db.models.signals import post_save, post_delete, pre_save, pre_delete
 from django.dispatch import receiver
-from django.core.cache import cache
-from rest_framework.authtoken.admin import User
-
-from confic import settings
 from confic.settings import BASE_DIR
 from shop.models import Product, Category
 import json
-
-
-@receiver(post_save, sender=Product)
-@receiver(post_save, sender=Category)
-def send_notification_email(sender, instance, created, **kwargs):
-    if created:
-        if sender == Product:
-            subject = 'new product has been created'
-            message = f'new product has been created: {instance.name}'
-        elif sender == Category:
-            subject = 'new category has been created'
-            message = f'new category has been created: {instance.title}'
-
-    recipient_list = User.objects.values_list('email', flat=True)
-
-    send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list)
-
-
-@receiver(post_delete, sender=Product)
-@receiver(post_delete, sender=Category)
-def send_notification_on_delete(sender, instance, **kwargs):
-    if sender == Product:
-        subject = 'The product has been deleted'
-        message = f'The product has been deleted: {instance.name}'
-    elif sender == Category:
-        subject = 'The category has been deleted'
-        message = f'The category has been deleted: {instance.title}'
-
-    recipient_list = User.objects.values_list('email', flat=True)
-    send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list)
-
 
 @receiver(pre_delete, sender=Product)
 def save_product_before_delete(sender, instance, **kwargs):
